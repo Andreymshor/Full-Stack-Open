a5f7f3c3d9304da4aac1042ee502b48f140d22fd
Andreymshor <andreyshor1999@gmail.com>
2022-10-10 20:25:26 -0400

    finished part 1

--- Course 1 - Introduction to React/part1/src/App.js
+++ Course 1 - Introduction to React/part1/src/App.js
@@ -1,17 +1,37 @@
 // This file defines a component with the name App
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  // any javascript code with the curly braces is evaluated and result is embedded into defined place in HTML produced by component
-  return(
-  <div>
-    <p>Hello world, it is {now.toString()}</p>
-    <p>{a} plus {b} is {a + b}</p>
-  </div>
-  )
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   // any javascript code with the curly braces is evaluated and result is embedded into defined place in HTML produced by component
+//   return(
+//   <div>
+//     <p>Hello world, it is {now.toString()}</p>
+//     <p>{a} plus {b} is {a + b}</p>
+//   </div>
+//   )
 
+// }
+
+// react components must be capitalized
+const Hello = (props) => { // we are able to nest components
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
+    </div>
+  )
 }
 
+const App = () => {
+  const name ='Peter'
+  const age = 10
+  return ( // Like so:
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10}/>
+      <Hello name={name} age={age}/> 
+    </>
+  )
+}
 
 export default App;
